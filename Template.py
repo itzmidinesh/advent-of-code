3fe24dc65e4d5dae291bd970a56512280210748e
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-12-06 12:41:15 +0530

    Update Template.py to include timeit

--- Template.py
+++ Template.py
@@ -1,6 +1,4 @@
-with open("input.txt", "r", encoding="utf8") as file:
-    data_input = file.readlines()
-data = [x.strip() for x in data_input]
+import timeit
 
 
 def part1(data):
@@ -13,4 +11,23 @@ def part2(data):
     return 0
 
 
+with open("input.txt", "r", encoding="utf8") as file:
+    data_input = file.readlines()
+data = [x.strip() for x in data_input]
+
 print(f"The solution for part 1 is {part1(data)} and part 2 is {part2(data)}")
+
+
+# Measure the execution time using timeit
+def measure_time(func, args):
+    return timeit.timeit(
+        stmt=f"{func}({args})", setup=f"from __main__ import {func}, {args}", number=1
+    )
+
+
+if __name__ == "__main__":
+    time_part1 = measure_time("part1", "data")
+    time_part2 = measure_time("part2", "data")
+
+    print(f"Time taken to execute part 1: {time_part1:.6f} seconds")
+    print(f"Time taken to execute part 2: {time_part2:.6f} seconds")
