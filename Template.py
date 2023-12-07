5a43a8e42d3613be491389d8bab53e781cca03d4
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-12-07 15:59:03 +0530

    Update Template.py to handle Input filepath error

--- Template.py
+++ Template.py
@@ -1,4 +1,17 @@
-import timeit
+import timeit, sys
+
+
+def read_data(file_path):
+    """
+    Read data from the input file and return a list of stripped lines.
+    """
+    try:
+        with open(file_path, "r", encoding="utf8") as file:
+            data_input = file.readlines()
+        return [x.strip() for x in data_input]
+    except FileNotFoundError:
+        print(f"Error: Input file {file_path} not found.")
+        sys.exit(1)
 
 
 def part1(data):
@@ -11,14 +24,6 @@ def part2(data):
     return 0
 
 
-with open("input.txt", "r", encoding="utf8") as file:
-    data_input = file.readlines()
-data = [x.strip() for x in data_input]
-
-print(f"The solution for part 1 is {part1(data)} and part 2 is {part2(data)}")
-
-
-# Measure the execution time using timeit
 def measure_time(func, args):
     return timeit.timeit(
         stmt=f"{func}({args})", setup=f"from __main__ import {func}, {args}", number=1
@@ -26,6 +31,11 @@ def measure_time(func, args):
 
 
 if __name__ == "__main__":
+    data = read_data("input.txt")
+    result_part1 = part1(data)
+    result_part2 = part2(data)
+    print(f"The solution for part 1 is {result_part1} and part 2 is {result_part2}")
+
     time_part1 = measure_time("part1", "data")
     time_part2 = measure_time("part2", "data")
 
